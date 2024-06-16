e226f5299d13d367c6eeda21fc112cf8f29f8adc
Hafeez <96433450+mhafeezajh@users.noreply.github.com>
2024-06-15 21:33:25 -0500

    Update Nav.vue

--- Code/client/src/components/Nav.vue
+++ Code/client/src/components/Nav.vue
@@ -11,7 +11,7 @@
                 class="ml-2"
                 to="/main"
                 style="color:white"
-                >Full Stack Demo with Amazon ECS + DevOps practices
+                >Amazon ECS
             </router-link>
         </b-navbar-brand>
 
@@ -37,19 +37,6 @@
                     </b-nav-item>
                 </b-navbar-nav>
 
-                <b-nav-item-dropdown text="My Orders and lists" right>
-                    <b-dropdown-item disabled>Wishlist</b-dropdown-item>
-                    <b-dropdown-item disabled>Orders</b-dropdown-item>
-                    <b-dropdown-item disabled>Send a gift!</b-dropdown-item>
-                </b-nav-item-dropdown>
-
-                <b-nav-item-dropdown right>
-                    <template v-slot:button-content>
-                        <em>User</em>
-                    </template>
-                    <b-dropdown-item disabled>Profile</b-dropdown-item>
-                    <b-dropdown-item to='/'>Sign Out</b-dropdown-item>
-                </b-nav-item-dropdown>
             </b-navbar-nav>
         </b-collapse>
     </b-navbar>
